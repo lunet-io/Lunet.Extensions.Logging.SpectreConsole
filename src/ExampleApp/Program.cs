59ed1be2ec49a6e0ec5d2332aecfa18e52a1db32
Alexandre Mutel <alexandre_mutel@live.com>
2022-04-02 16:25:32 +0200

    Trigger a ci

--- src/ExampleApp/Program.cs
+++ src/ExampleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using Lunet.Extensions.Logging.SpectreConsole;
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+using Lunet.Extensions.Logging.SpectreConsole;
 using Microsoft.Extensions.Logging;
 using Spectre.Console;
 
